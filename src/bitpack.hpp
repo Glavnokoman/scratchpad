c22b5a20dc37269dfed25dbfa516d626ced64fb6
Slava <s.v.savenko@gmail.com>
2017-11-08 09:24:47 +0100

    bits (wip)

--- src/bitpack.hpp
+++ src/bitpack.hpp
@@ -4,6 +4,8 @@
 #include <cassert>
 #include <cstdint>
 
+#include <iostream> // debug
+
 namespace detail {
 	template<uint8_t val>
 	struct TypeBits {
@@ -21,103 +23,90 @@ namespace detail {
 	
 	template<uint8_t bits> using Uint_t = typename Uint<TypeBits<bits>::value>::type;
 
-	template<uint16_t begin, uint16_t end, class Res=Uint_t<end-begin>>
-	struct BitsProxy{ 
-		uint8_t* const buf;
-		
-		explicit BitsProxy(uint8_t* buf): buf{buf}{}
-		
-		operator Res() const {
-			return Res{};
-		}
-	};
+	/// @return true if begin and end of the bit range values are within same byte
+	template<uint8_t begin, uint8_t end>
+	constexpr static auto samebyte() { return begin/8u == end/8u; }
 	
-	template<uint16_t begin, uint16_t end, class Res=Uint_t<end-begin>>
-	struct ConstBitsProxy{
-		const uint8_t* const buf;
+	///
+	template<class Buf, uint16_t begin, uint16_t end, class Res=Uint_t<end-begin>>
+	struct BitsProxy_{ 
+		const Buf buf;
 		
-		explicit ConstBitsProxy(const uint8_t* buf): buf{buf}{}
+		explicit BitsProxy_(Buf buf): buf{buf}{}
 		
-		operator Res() const { return Res{}; }
-	};
+		///
+		operator Res() const {
+			auto b0 = buf + begin/8;
+			
+			if constexpr (samebyte<begin, end>()){ // begin and end are in the same byte
+				constexpr auto mask = (1u << (end - begin)) - 1u;
+				return (*b0 >> (8 - end%8)) & mask;
+			}
+	
+			Res r = (begin%8 == 0 ? *b0 : (*b0) & (255u >> begin%8));
+			
+			constexpr auto bytebegin = (begin + 8u) & ~7u;     // begin of the next read aligned at byte boundary
+			for(auto i = (end - bytebegin)/8u ; i != 0u; --i){ // read up complete bytes
+				r = (r << 8);
+				r |= *(++b0);
+			}
+	
+			if constexpr (end%8 != 0){ // end is not aligned at byte boundary
+				r = (r << end%8);
+				r |= *(++b0) >> (8 - end%8);
+			}
+			return r;
+		}
+		
+		///
+		auto operator= (Res x)-> void {
+			assert(x <= (Res(-1) >> (8*sizeof(Res) - (end - begin)))); // number fits into designated bits
+			
+			if constexpr (samebyte<begin, end>()){ // begin and end are in the same byte
+				constexpr auto mask = (255u >> begin%8);
+				buf[begin/8] |= (uint8_t(x) << (8 - end%8)) & mask;
+				return;
+			}
+			
+			auto b0 = buf + (end + 7)/8; // end of the next byte to write
+			if constexpr (end%8 != 0){ // write the last bits not making a complete byte
+				*(--b0) |= (uint8_t(x) << (8 - end%8));
+				x >>= end%8;
+			}
+	
+			constexpr auto byteend = end & ~7u; // end of the next write aligned at byte boundary
+			if constexpr ((byteend - begin) > 8u){
+				for(auto i = (byteend - begin)/8u; i != 0; --i){ // write complete bytes
+					*(--b0) = uint8_t(x);
+					x >>= 8;
+				}
+			}
+			
+			if constexpr (begin%8 != 0){ *(--b0) |= uint8_t(x); } // begin is not aligned at byte boundary
+		}
+		
+		///
+		template<class T>
+		friend auto operator== (BitsProxy_ x, T val)-> bool { return Res(x) == val; }
+	}; // struct BitsProxy_
+	
+	template<uint16_t begin, uint16_t end>
+	using BitsProxy = BitsProxy_<uint8_t*, begin, end, Uint_t<end-begin>>;
+	
+	template<uint16_t begin, uint16_t end>
+	using ConstBitsProxy = BitsProxy_<uint8_t const*, begin, end, Uint_t<end-begin>>;
 }
 
 template<uint16_t begin, uint16_t end> struct BitRange{ enum{Begin=begin, End=end};};
 
 template<class T>
-auto bits(const uint8_t* buf) { 
+auto bits(const uint8_t* buf) {
+	std::cout << __PRETTY_FUNCTION__ << "\n";
 	return detail::ConstBitsProxy<T::Begin, T::End>{buf}; 
 }
 
 template<class T>
 auto bits(uint8_t* buf) {
+	std::cout << __PRETTY_FUNCTION__ << "\n";
 	return detail::BitsProxy<T::Begin, T::End>{buf};
 }
-
-
-/// big-endian bitpack
-template<uint16_t begin, uint16_t end=begin+1u, class Res = detail::Uint_t<end-begin>> 
-struct bitpack{
-	uint8_t* buf;
-	const uint8_t* cbuf;
-
-	explicit bitpack(uint8_t* buf): buf(buf), cbuf(buf) {}
-	explicit bitpack(const uint8_t* cbuf): buf(nullptr), cbuf(cbuf) {}
-
-	operator Res() const {
-		auto b0 = cbuf + begin/8;
-		
-		if constexpr (samebyte()){ // begin and end are in the same byte
-			constexpr auto mask = (1u << (end - begin)) - 1u;
-			return (*b0 >> (8 - end%8)) & mask;
-		}
-
-		Res r = (begin%8 == 0 ? *b0 : (*b0) & (255u >> begin%8));
-		
-		constexpr auto bytebegin = (begin + 8u) & ~7u;     // begin of the next read aligned at byte boundary
-		for(auto i = (end - bytebegin)/8u ; i != 0u; --i){ // read up complete bytes
-			r = (r << 8);
-			r |= *(++b0);
-		}
-
-		if constexpr (end%8 != 0){ // end is not aligned at byte boundary
-			r = (r << end%8);
-			r |= *(++b0) >> (8 - end%8);
-		}
-		return r;
-	}
-	
-//	template<class T>
-	auto operator=(Res x)-> void {
-		assert(x <= (Res(-1) >> (8*sizeof(Res) - (end - begin)))); // number fits into designated bits
-		
-		if constexpr (samebyte()){ // begin and end are in the same byte
-			constexpr auto mask = (255u >> begin%8);
-			buf[begin/8] |= (uint8_t(x) << (8 - end%8)) & mask;
-			return;
-		}
-		
-		auto b0 = buf + (end + 7)/8; // end of the next byte to write
-		if constexpr (end%8 != 0){ // write the last bits not making a complete byte
-			*(--b0) |= (uint8_t(x) << (8 - end%8));
-			x >>= end%8;
-		}
-
-		constexpr auto byteend = end & ~7u;              // end of the next write aligned at byte boundary
-		if constexpr ((byteend - begin) > 8u){
-			for(auto i = (byteend - begin)/8u; i != 0; --i){ // write complete bytes
-				*(--b0) = uint8_t(x);
-				x >>= 8;
-			}
-		}
-		
-		if constexpr (begin%8 != 0){ *(--b0) |= uint8_t(x); } // begin is not aligned at byte boundary
-	}
-	
-	template<class T>
-	auto friend operator == (bitpack b, T val)-> bool { return Res(b) == val; }
-	
-private: // helpers
-	/// @return true if bitpack values are within one byte boundaries
-	constexpr static auto samebyte() { return begin/8u == end/8u; }
-};
--- test/bitpack_t.cpp
+++ test/bitpack_t.cpp
@@ -4,81 +4,101 @@
 #include "catch/catch.hpp"
 
 namespace {
-	struct H {
+//	struct H {
+//		enum {size = 64};
+		
+//		using f0_8  = bitpack<0, 8>;
+//		using f0_1  = bitpack<0, 1>;
+//		using f6_7  = bitpack<6>;
+//		using f5_8  = bitpack<5, 8>;
+//		using f6_9  = bitpack<6, 9>;
+//		using f7_8  = bitpack<7, 8> ;
+//		using f5_15 = bitpack<5, 15>;
+//		using f1_17 = bitpack<1, 17>;
+//		using f0_32 = bitpack<0, 32>;
+//		using f35_40 = bitpack<35, 40>;
+//		using f32_64 = bitpack<32, 64>;
+//		using f64_88 = bitpack<64, 88>;
+//	};
+	
+	struct G {
 		enum {size = 64};
 		
-		using f0_8  = bitpack<0, 8>;
-		using f0_1  = bitpack<0, 1>;
-		using f6_7  = bitpack<6>;
-		using f5_8  = bitpack<5, 8>;
-		using f6_9  = bitpack<6, 9>;
-		using f7_8  = bitpack<7, 8> ;
-		using f5_15 = bitpack<5, 15>;
-		using f1_17 = bitpack<1, 17>;
-		using f0_32 = bitpack<0, 32>;
-		using f35_40 = bitpack<35, 40>;
-		using f32_64 = bitpack<32, 64>;
-		using f64_88 = bitpack<64, 88>;
+		using f0_8 = BitRange<0, 8>;
 	};
 	
-	uint8_t cbuf[12] = { 0b01001010, 0b00110101, 0b11101100, 0b00101101
+	const uint8_t cbuf[12] = { 0b01001010, 0b00110101, 0b11101100, 0b00101101
 	                   , 0b01001010, 0b00110101, 0b11101100, 0b00101101
 	                   , 0b01001010, 0b00110101, 0b11101100, 0b00101101 };
 	uint8_t buf[12] = {0u};
 } // namespace name
 
-TEST_CASE("read bitfields", "[read_bitfields]"){
-	CHECK(H::f0_8(cbuf) == cbuf[0]);
-	CHECK(H::f0_1(cbuf) == false);
-	CHECK(H::f6_7(cbuf) == true);
-	CHECK(H::f5_8(cbuf) == 0b010u);
-	CHECK(H::f6_9(cbuf) == 0b100u);
-	CHECK(H::f7_8(cbuf) == false);
-	CHECK(H::f5_15(cbuf) == 0b0100011010u);
-	CHECK(H::f1_17(cbuf) == 0b1001010001101011u);
-	CHECK(H::f0_32(cbuf) == 0b01001010001101011110110000101101u);
-	CHECK(H::f35_40(cbuf) == 0b01010u);
-	CHECK(H::f32_64(cbuf) == 0b01001010001101011110110000101101u);
-	CHECK(H::f64_88(cbuf) == 0b010010100011010111101100u);
+TEST_CASE("read bitranges", "[read_bitfoelds"){
+	const uint8_t cbuf[12] = { 0b01001010, 0b00110101, 0b11101100, 0b00101101
+	                         , 0b01001010, 0b00110101, 0b11101100, 0b00101101
+	                         , 0b01001010, 0b00110101, 0b11101100, 0b00101101 };
+	uint8_t buf[12] = {0u};
+
+	CHECK(bits<G::f0_8>(cbuf) == cbuf[0]);
+	
+	bits<G::f0_8>(cbuf) = 0;
+	
+	bits<G::f0_8>(buf) = cbuf[0];
+	CHECK(bits<G::f0_8>(buf) == cbuf[0]);
 }
 
-TEST_CASE("write bitfields", "[write_bitfields]"){
-	H::f0_8{buf} = cbuf[0];
-	CHECK(H::f0_8{buf} == cbuf[0]);
-	
-	H::f0_1{buf} = false;
-	CHECK(H::f0_1(buf) == false);
+//TEST_CASE("read bitfields", "[read_bitfields]"){
+//	CHECK(H::f0_8(cbuf) == cbuf[0]);
+//	CHECK(H::f0_1(cbuf) == false);
+//	CHECK(H::f6_7(cbuf) == true);
+//	CHECK(H::f5_8(cbuf) == 0b010u);
+//	CHECK(H::f6_9(cbuf) == 0b100u);
+//	CHECK(H::f7_8(cbuf) == false);
+//	CHECK(H::f5_15(cbuf) == 0b0100011010u);
+//	CHECK(H::f1_17(cbuf) == 0b1001010001101011u);
+//	CHECK(H::f0_32(cbuf) == 0b01001010001101011110110000101101u);
+//	CHECK(H::f35_40(cbuf) == 0b01010u);
+//	CHECK(H::f32_64(cbuf) == 0b01001010001101011110110000101101u);
+//	CHECK(H::f64_88(cbuf) == 0b010010100011010111101100u);
+//}
 
-	H::f6_7{buf} = true;
-	CHECK(H::f6_7(buf) == true);
+//TEST_CASE("write bitfields", "[write_bitfields]"){
+//	H::f0_8{buf} = cbuf[0];
+//	CHECK(H::f0_8{buf} == cbuf[0]);
 	
-	H::f5_8{buf} = 0b010u;
-	CHECK(H::f5_8(buf) == 0b010u);
-	
-	H::f6_9{buf} = 0b100u;
-	CHECK(H::f6_9(buf) == 0b100u);
-	
-	H::f7_8{buf} = false;
-	CHECK(H::f7_8(buf) == false);
+//	H::f0_1{buf} = false;
+//	CHECK(H::f0_1(buf) == false);
 
-	H::f5_15{buf} = 0b0100011010u;
-	CHECK(H::f5_15(buf) == 0b0100011010u);
+//	H::f6_7{buf} = true;
+//	CHECK(H::f6_7(buf) == true);
 	
-	H::f1_17{buf} = 0b1001010001101011u;
-	CHECK(H::f1_17(buf) == 0b1001010001101011u);
+//	H::f5_8{buf} = 0b010u;
+//	CHECK(H::f5_8(buf) == 0b010u);
 	
-	H::f0_32{buf} = 0b01001010001101011110110000101101u;
-	CHECK(H::f0_32(buf) == 0b01001010001101011110110000101101u);
+//	H::f6_9{buf} = 0b100u;
+//	CHECK(H::f6_9(buf) == 0b100u);
 	
-	H::f35_40{buf} = 0b01010u;
-	CHECK(H::f35_40(buf) == 0b01010u);
+//	H::f7_8{buf} = false;
+//	CHECK(H::f7_8(buf) == false);
+
+//	H::f5_15{buf} = 0b0100011010u;
+//	CHECK(H::f5_15(buf) == 0b0100011010u);
 	
-	H::f32_64{buf} = 0b01001010001101011110110000101101u;
-	CHECK(H::f32_64(buf) == 0b01001010001101011110110000101101u);
+//	H::f1_17{buf} = 0b1001010001101011u;
+//	CHECK(H::f1_17(buf) == 0b1001010001101011u);
 	
-	H::f64_88{buf} = 0b010010100011010111101100u;
-	CHECK(H::f64_88(buf) == 0b010010100011010111101100u);
-}
+//	H::f0_32{buf} = 0b01001010001101011110110000101101u;
+//	CHECK(H::f0_32(buf) == 0b01001010001101011110110000101101u);
+	
+//	H::f35_40{buf} = 0b01010u;
+//	CHECK(H::f35_40(buf) == 0b01010u);
+	
+//	H::f32_64{buf} = 0b01001010001101011110110000101101u;
+//	CHECK(H::f32_64(buf) == 0b01001010001101011110110000101101u);
+	
+//	H::f64_88{buf} = 0b010010100011010111101100u;
+//	CHECK(H::f64_88(buf) == 0b010010100011010111101100u);
+//}
 
 int main( int argc, char* argv[] )
 {
